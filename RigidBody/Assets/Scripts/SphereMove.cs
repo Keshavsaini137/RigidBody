0148f186904ac1dc49306b0d2c7957572b210d09
Keshavsaini137 <Keshavsaini137@gmail.com>
2018-03-20 18:45:28 +0530

    new Project

---
+++ RigidBody/Assets/Scripts/SphereMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMove : MonoBehaviour {
+
+    //public GameObject sphere;
+
+	// Use this for initialization
+	void Start () {
+       //this.transform.position(0,20,9);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
